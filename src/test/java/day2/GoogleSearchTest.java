a3b90e04d8fe41a623aba3aaeba58840c8d59997
Mesut Butun <mstbtn@gmail.com>
2020-03-01 14:42:20 -0800

    basic navigation create

---
+++ src/test/java/day2/GoogleSearchTest.java
@@ -0,0 +1,25 @@
+package day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearchTest {
+    public static void main(String[] args) throws Exception{
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.get("http://google.com");
+        Thread.sleep(2000);
+        // By.name("q")---->name="q"
+
+        WebElement search= driver.findElement(By.name("q"));
+
+        search.sendKeys("Java", Keys.ENTER);
+        Thread.sleep(2000);
+
+        driver.quit();
+    }
+}
